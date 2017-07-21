031f7b728dd5dd1f2b34f20cd1d82af155a2204f
jenksy <jjenksy@gmail.com>
2017-07-21 15:32:51 -0500

    Initial commit

---
+++ src/main/java/io/logicode/LogicodeApiApplication.java
@@ -0,0 +1,12 @@
+package io.logicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicodeApiApplication.class, args);
+	}
+}
---
+++ src/test/java/io/logicode/LogicodeApiApplicationTests.java
@@ -0,0 +1,16 @@
+package io.logicode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogicodeApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
